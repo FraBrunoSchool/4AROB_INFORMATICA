59885d4e5e8b847193d90d8406f6a751aa90031e
Francesco Bruno <46680491+FraBrunoSchool@users.noreply.github.com>
2020-01-02 16:31:10 +0100

    funzionante

--- PROCESSING_IN_JAVA/ES_001_INFINITEPALLINE/src/Inizio.java
+++ PROCESSING_IN_JAVA/ES_001_INFINITEPALLINE/src/Inizio.java
@@ -1,35 +1,45 @@
 import processing.core.PApplet;
-
 import java.awt.*;
+import java.util.Random;
 
 public class Inizio extends PApplet {
     private Cerchio c;
+    /*
+        2160p: 3840 x 2160
+        1440p: 2560 x 1440
+        1080p: 1920 x 1080
+        720p: 1280 x 720
+        480p: 854 x 480
+        360p: 640 x 360
+        240p: 426 x 240
+    */
+    private int DIM_X = 854;
+    private int DIM_Y = 480;
     // The argument passed to main must match the class name
     public static void main(String[] args) {
         PApplet.main("Inizio");
     }
 
     // method used only for setting the size of the window
-
     public void settings(){
-        size(640, 360);
+        size(DIM_X, DIM_Y);
     }
 
     // runs once at the beginning
-
     public void setup(){
-        background(25,55,2);
+        background(255,255,255);
         c = new Cerchio(this); //gli passa la PApplet
     }
 
     // runs over and over again as long as the program is still running
-
     public void draw(){
 
-        c.setCordX(random(640));
-        c.setCordY(random(360));
+        Random rand = new Random();
+        c.setCordX(random(DIM_X));
+        c.setCordY(random(DIM_Y));
         c.show();
+        c.setColore(new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()));
+        c.setBordo(new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()));
         delay(100);
-        //c.setColore(new Color(random(256), random(256), random(256)));
     }
 }
